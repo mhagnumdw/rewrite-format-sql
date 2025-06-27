22c79338390a3a6a006ea94a0bafe604db021d76
Dwouglas Mhagnum <dwouglas@gmail.com>
2025-06-26 21:14:13 -0300

    chore: update release script with Sonatype publishing link

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # // TODO: deletar esse arquivo após ter configurado o processo de release completamente
+# https://central.sonatype.org/publish/publish-portal-maven/
 
 ./mvnw -V -B release:clean release:prepare -DreleaseVersion=1.0.0
 
