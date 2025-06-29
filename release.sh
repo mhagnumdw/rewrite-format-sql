f48da5e572c605547755c8d325c574d2f7b9e630
Dwouglas Mhagnum <dwouglas@gmail.com>
2025-06-29 18:38:04 -0300

    chore: remove obsolete release script after process configuration

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# // TODO: deletar esse arquivo após ter configurado o processo de release completamente
-# https://central.sonatype.org/publish/publish-portal-maven/
-
-./mvnw -V -B release:clean release:prepare -DreleaseVersion=1.0.0
-
-./mvnw -V release:perform
